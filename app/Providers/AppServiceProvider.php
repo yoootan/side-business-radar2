fd9d02fb81746ed5afa276b597a58aaaa2108988
yoootan <59093014+yoootan@users.noreply.github.com>
2020-10-15 17:50:33 +0900

    heroku3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(IdeHelperServiceProvider::class);
     }
+}
 
     /**
      * Bootstrap any application services.
